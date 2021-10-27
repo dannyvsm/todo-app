8570c218c2fb5b9fe29b5c9784c6eadd468bb295
dannyvsm <31244585+dannyvsm@users.noreply.github.com>
2021-10-27 18:55:57 +0100

    additional styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import { BsSunFill } from "react-icons/bs";
+import { BsMoonStarsFill } from "react-icons/bs";
+import { FaTree } from "react-icons/fa";
 
 function Header() {
-    return (
-        <div class="head">
-            <h2>hello</h2>
-            <p>this is useful</p>
-            
-        </div>
-    )
+  return (
+    <div class="head">
+      <div className="timedate">
+        <h2>19:00</h2>
+        <p>manchester, 12°c</p>
+      </div>
+      <div class="theme-btns">
+        <BsSunFill className="light" />
+        <BsMoonStarsFill className="dark" />
+        <FaTree className="forest" />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
