b27d5847d3df9df29a16beb13c3206e248bb008c
dannyvsm <31244585+dannyvsm@users.noreply.github.com>
2021-10-27 18:41:14 +0100

    Updated components + icons

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,38 +1,35 @@
-import React, { useState } from 'react'
-import { RiDeleteBack2Line } from 'react-icons/ri'
-import { FiEdit2 } from 'react-icons/fi'
-import './todo.css'
+import React, { useState } from "react";
+import { RiDeleteBack2Line } from "react-icons/ri";
+import { FiEdit2 } from "react-icons/fi";
+import "./todo.css";
 
-function Todo({todos, completeTodo, removeTodo}) {
-    // const [edit, setEdit] = useState({
-    //     id: null,
-    //     value: ''
-    // })
+function Todo({ todos, completeTodo, removeTodo }) {
+  // const [edit, setEdit] = useState({
+  //     id: null,
+  //     value: ''
+  // })
 
-    return todos.map((todo, index) => (
-        <div
-            className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
-            key={index}
-        >
-            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-                {todo.text}
-            </div>
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
 
-            <div className="icons">
-                <RiDeleteBack2Line
-                    onClick={() => removeTodo(todo.id)}
-                    className='delete-icon'
-                />
-                {/* <FiEdit2
+      <div className="icons">
+        <RiDeleteBack2Line
+          onClick={() => removeTodo(todo.id)}
+          className="delete-icon"
+        />
+        {/* <FiEdit2
                     onClick={() => setEdit({ id: todo.id, value: todo.text})}
                     className='edit-icon'
                 /> */}
-            </div>
-
-
-
-        </div>
-    ))
+      </div>
+    </div>
+  ));
 }
 
-export default Todo
+export default Todo;
